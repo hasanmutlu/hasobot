1ce0efd6a09bc819dbdcfc7724f969f3faea17db
Hasan MUTLU <hasanmutlu9@gmail.com>
2019-09-07 18:34:20 +0300

    update

--- Main.py
+++ Main.py
@@ -4,10 +4,13 @@ from Core.Util.Util import Util
 from Core.TelegramBotManager import TelegramBotManager
 
 if __name__ == '__main__':
-    program_path = os.path.dirname(__file__)
-    if program_path is not '':
-        os.chdir(program_path)
-    logging.basicConfig(filename=Util.get_abs_file_name("hasobot.log"),
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    TelegramBotManager().initialize()
+    try:
+        program_path = os.path.dirname(__file__)
+        if program_path is not '':
+            os.chdir(program_path)
+        logging.basicConfig(filename=Util.get_abs_file_name("hasobot.log"),
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        TelegramBotManager().initialize()
+    except Exception as e:
+        logging.error(str(e))
